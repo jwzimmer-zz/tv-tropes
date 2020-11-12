7c93228ea429ddafa194a5c3352bf0cb0a994d7c
jwzimmer <jzimmer1@uvm.edu>
2020-11-11 21:17:09 -0500

    average number of tropes in common between indices: 76.06801850713087

--- indices_clusters.py
+++ indices_clusters.py
@@ -17,7 +17,7 @@ class IndexGraph():
     def __init__(self):
         self.G = nx.Graph()
         self.masterlist = self.get_json('index-list/index-list.json')
-        self.go_thru_indices(5000)
+        self.go_thru_indices_threshold()
     
     def write_gml(self,G,name):
         nx.write_gml(G, name+".gml")
@@ -50,6 +50,22 @@ class IndexGraph():
         #self.write_gml(self.G,"indices")
         return self.G
     
+    def go_thru_indices_threshold(self):
+        i = 0
+        avgnumberlinks = 93.82385016595543 #only looking at indices with above the average number of links still yields a graph with 26747 nodes
+        for index in self.masterlist:
+            indexlinks = set(self.masterlist[index])
+            self.G.add_node(index,tropes=indexlinks)
+        for idx, node in enumerate(self.G.nodes()):
+            for idy, node2 in enumerate(self.G.nodes()):
+                if idx != idy:
+                    scoreval = len(list(self.G.nodes[node]["tropes"].intersection(self.G.nodes[node]["tropes"])))
+                    self.G.add_edge(node, node2, score=scoreval)
+        for idx,node in enumerate(self.G.nodes()):
+            del self.G.nodes[node]["tropes"]
+        self.write_gml(self.G,"indices_common_trope_score")
+        return self.G
+    
     def basic_analysis(self, k):
         girvannewman = nx.algorithms.community.centrality.girvan_newman(self.G)
         limited = itertools.takewhile(lambda c: len(c) <= k, girvannewman)
@@ -60,6 +76,6 @@ class IndexGraph():
         return None
 
 i = IndexGraph()
-i.basic_analysis(6)
+#i.basic_analysis(6)
 
     
