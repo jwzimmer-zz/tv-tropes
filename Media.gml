12826bc2d9f1e8ed44ef99f7ac90d48226147e6d
jwzimmer <jzimmer1@uvm.edu>
2020-11-14 14:32:39 -0500

    saved some hairball examples, started saving individual indices so we can see what's going on

---
+++ Media.gml
@@ -0,0 +1,190 @@
+graph [
+  node [
+    id 0
+    label "Media"
+  ]
+  node [
+    id 1
+    label "Vaudeville"
+  ]
+  node [
+    id 2
+    label "Advertising"
+  ]
+  node [
+    id 3
+    label "AdvertisingCampaigns"
+  ]
+  node [
+    id 4
+    label "Feelies"
+  ]
+  node [
+    id 5
+    label "ConceptAlbum"
+  ]
+  node [
+    id 6
+    label "DeckbuildingGame"
+  ]
+  node [
+    id 7
+    label "ParlorGames"
+  ]
+  node [
+    id 8
+    label "CardGames"
+  ]
+  node [
+    id 9
+    label "AlternateRealityGame"
+  ]
+  node [
+    id 10
+    label "WebSerialNovel"
+  ]
+  node [
+    id 11
+    label "VisualNovel"
+  ]
+  node [
+    id 12
+    label "UgandanMedia"
+  ]
+  node [
+    id 13
+    label "BroadcastLive"
+  ]
+  node [
+    id 14
+    label "PrintMedia"
+  ]
+  node [
+    id 15
+    label "ShortFilm"
+  ]
+  node [
+    id 16
+    label "Newsreel"
+  ]
+  node [
+    id 17
+    label "LARP"
+  ]
+  node [
+    id 18
+    label "Legend"
+  ]
+  node [
+    id 19
+    label "TheWikiRule"
+  ]
+  node [
+    id 20
+    label "Podcast"
+  ]
+  node [
+    id 21
+    label "Machinomics"
+  ]
+  node [
+    id 22
+    label "WarGaming"
+  ]
+  node [
+    id 23
+    label "VHSGame"
+  ]
+  edge [
+    source 0
+    target 1
+  ]
+  edge [
+    source 0
+    target 2
+  ]
+  edge [
+    source 0
+    target 3
+  ]
+  edge [
+    source 0
+    target 4
+  ]
+  edge [
+    source 0
+    target 5
+  ]
+  edge [
+    source 0
+    target 6
+  ]
+  edge [
+    source 0
+    target 7
+  ]
+  edge [
+    source 0
+    target 8
+  ]
+  edge [
+    source 0
+    target 9
+  ]
+  edge [
+    source 0
+    target 10
+  ]
+  edge [
+    source 0
+    target 11
+  ]
+  edge [
+    source 0
+    target 12
+  ]
+  edge [
+    source 0
+    target 13
+  ]
+  edge [
+    source 0
+    target 14
+  ]
+  edge [
+    source 0
+    target 15
+  ]
+  edge [
+    source 0
+    target 16
+  ]
+  edge [
+    source 0
+    target 17
+  ]
+  edge [
+    source 0
+    target 18
+  ]
+  edge [
+    source 0
+    target 19
+  ]
+  edge [
+    source 0
+    target 20
+  ]
+  edge [
+    source 0
+    target 21
+  ]
+  edge [
+    source 0
+    target 22
+  ]
+  edge [
+    source 0
+    target 23
+  ]
+]
---
+++ indices_smallviz.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 26 2020
+
+@author: jzimmer1, philnguyen
+"""
+import os
+import json
+from networkx import algorithms
+from networkx import community
+from networkx import centrality
+import networkx as nx
+import itertools
+import matplotlib.pyplot as plt
+
+class IndexGraph():
+    def __init__(self):
+        self.G = nx.Graph()
+        self.masterlist = self.get_json('index-list/index-list.json')
+        self.indices = [x for x in self.masterlist.keys()]
+        #truncated list for testing things quickly or on a subset of indices
+        #self.masterlist = {self.indices[i]:self.masterlist[self.indices[i]] for i in range(len(self.masterlist)) if self.indices[i] in ("Media","MediaAdaptationTropes","MediaTropes","MediaWatchdog")}
+        self.masterlist = {self.indices[i]:self.masterlist[self.indices[i]] for i in range(len(self.masterlist)) if self.indices[i] in ("Media")}
+        self.add_trope_nodes()
+        self.go_thru_indices_sets()
+    
+    def write_gml(self,G,name):
+        nx.write_gml(G, name+".gml")
+        return None
+          
+    def get_json(self, filename):
+        with open(filename) as f:
+            jsonobj = json.load(f)
+        return jsonobj
+    
+    def write_json(self, data, filename):
+        with open(filename,"w") as f:
+            json.dump(data,f)
+        return None
+    
+    def show_graph(self):
+        pos = nx.spring_layout(self.G)
+        nx.draw(self.G, pos)
+        node_labels = nx.get_node_attributes(self.G,'label')
+        print(node_labels)
+        nx.draw_networkx_labels(self.G, pos, labels=node_labels)
+        return None
+    
+    def add_sets(self):
+        for index in self.masterlist: #add all linked tropes as a set
+            indexlinks = set(self.masterlist[index])
+            self.G.add_node(index,tropes=indexlinks)
+        return None
+    
+    def add_trope_nodes(self):
+        for index in self.masterlist: #add all linked tropes as nodes
+            indexlinks = set(self.masterlist[index])
+            self.G.add_node(index,label=index)
+            for trope in indexlinks:
+                self.G.add_node(trope,label=trope)
+                self.G.add_edge(index,trope)
+            self.write_gml(self.G, index)
+        return None
+    
+    def histogram_plot(self, datax, datay):
+        fig, axs = plt.subplots(1, 2, sharey=True, tight_layout=True)
+        axs[0].hist(datax, bins=100)
+        axs[1].hist(datay, bins=100)
+        plt.show()
+        return None
+
+    def go_thru_indices_sets(self):
+        for idx, node in enumerate(self.G.nodes()):
+            pass
+        return None
+
+i = IndexGraph()
+i.show_graph()
+
+    
